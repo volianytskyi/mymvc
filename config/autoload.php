461bfa7c8c10f73a5f31628cbbab68e3dc930a7d
Sergey Volyanytsky <sergey.volyanytsky@gmail.com>
2018-04-27 20:25:59 +0300

    added test index model, view and controller

--- config/autoload.php
+++ config/autoload.php
@@ -5,17 +5,17 @@ function classRegister($class)
   $classFile = '';
   if(preg_match('~.*Model$~', $class))
   {
-    $classFile = __DIR__ . '/../app/models/' . $class . 'php';
+    $classFile = __DIR__ . '/../app/models/' . $class . '.php';
   }
   elseif (preg_match('~.*View$~', $class))
   {
-    $classFile = __DIR__ . '/../app/views/' . $class . 'php';
+    $classFile = __DIR__ . '/../app/views/' . $class . '.php';
   }
   elseif (preg_match('~.*Controller$~', $class))
   {
-    $classFile = __DIR__ . '/../app/controllers/' . $class . 'php';
+    $classFile = __DIR__ . '/../app/controllers/' . $class . '.php';
   }
-
+  
   if(file_exists($classFile))
   {
     require_once($classFile);
